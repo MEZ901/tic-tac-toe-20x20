6834528d67ae9de08f7722c39a71884c8e291a6b
MEZ901 <issammez44@gmail.com>
2023-09-21 16:20:18 +0100

    change the ui of player setup page

--- src/layouts/PlayerSetupLayout.js
+++ src/layouts/PlayerSetupLayout.js
@@ -1,7 +1,11 @@
 import PlayerSetup from "../components/PlayerSetup";
 
 const PlayerSetupLayout = () => {
-  return PlayerSetup();
+  return `
+    <div class="bg-gray-200 w-full px-16 md:px-0 h-screen flex items-center justify-center">
+      ${PlayerSetup()}
+    </div>
+  `;
 };
 
 export default PlayerSetupLayout;
